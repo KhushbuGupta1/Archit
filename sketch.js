c6e72e62d406f4fb88d6920d49d7cb85b7215f21
KhushbuGupta1 <khushbuguptamit@gmail.com>
2021-05-20 20:27:21 +0530

    Add platform

--- sketch.js
+++ sketch.js
@@ -18,6 +18,8 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
 
+    platform = new Ground (150,300,300,180)
+
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -34,7 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    
 }
 
 function draw(){
@@ -59,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
